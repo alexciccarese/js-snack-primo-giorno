9e43362d9717d7b31db355e863ba00ac8f0de4e2
alexciccarese <alex.ciccarese@gmail.com>
2025-02-13 11:43:41 +0100

    fix print text

--- snack-2/main.js
+++ snack-2/main.js
@@ -5,7 +5,7 @@ const word1 = prompt('Inserisci prima parola')
 const word2 = prompt('Inserisci seconda parola')
 
 if (word1.length > word2.length) {
-  console.log(`La parola ${word2} è più lunga di ${word1}`);
+  console.log(`La parola ${word2} è più corta di ${word1}`);
 
 } else if (word1.length < word2.length) {
   console.log(`La parola ${word1} è più corta di ${word2}`);
