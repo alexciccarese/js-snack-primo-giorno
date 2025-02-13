6e46502e939f779659d2e692ef903931131155d7
alexciccarese <alex.ciccarese@gmail.com>
2025-02-13 11:22:10 +0100

    add exercise 2

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,2 +1,18 @@
 /* L'utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga */
+
+const word1 = prompt('Inserisci prima parola')
+const word2 = prompt('Inserisci seconda parola')
+
+if (word1.length > word2.length) {
+  console.log(`La parola ${word1} è più lunga di ${word2}`);
+
+} else if (word1.length < word2.length) {
+  console.log(`La parola ${word1} è più corta di ${word2}`);
+
+} else {
+  console.log(`Le parole ${word1} e ${word2} hanno la stessa lunghezza`);
+  
+}
+
+  
