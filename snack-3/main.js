c8b3706c2037ee4ed7f1759d6f5078a944541fa1
alexciccarese <alex.ciccarese@gmail.com>
2025-02-13 12:22:30 +0100

    add 3 exercise

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,2 +1,15 @@
 /* Il software deve chiedere per 10 volte all'utente di inserire un numero.
 Il programma stammpa la somma di tutti i numeri */
+
+let somma = 0
+
+for (let i = 0; i < 10; i++) {
+  const numero = prompt('Inserisci un numero:')
+  
+  somma += +numero
+}
+
+console.log(somma);
+
+
+
